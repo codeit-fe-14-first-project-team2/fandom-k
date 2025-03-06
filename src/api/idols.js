eb0534e96758c29461c25dc402a31f4ae9f66fbb
callu9 <callu_9ine@naver.com>
2025-03-06 19:14:46 +0900

    chore: api teamName 설정

--- src/api/idols.js
+++ src/api/idols.js
@@ -1,14 +1,14 @@
 import { commonInstance, HTTP_STATUS, teamName } from "./common-http";
 
 export async function getIdolList(keyword, cursor = 1, pageSize = 10) {
-  const params = { keyword, cursor, pageSize };
-  try {
-    const response = await commonInstance.get(`${teamName}/idols`, { params });
-    if (response.status === HTTP_STATUS.STATUS_OK) return response.data;
-  } catch (err) {
-    console.log(err);
-    throw new Error("아이돌 목록 조회 중 오류가 발생했습니다.");
-  }
+	const params = { keyword, cursor, pageSize };
+	try {
+		const response = await commonInstance.get(`/${teamName}/idols`, { params });
+		if (response.status === HTTP_STATUS.STATUS_OK) return response.data;
+	} catch (err) {
+		console.log(err);
+		throw new Error("아이돌 목록 조회 중 오류가 발생했습니다.");
+	}
 }
 /**
  *
@@ -28,11 +28,11 @@ export async function getIdolList(keyword, cursor = 1, pageSize = 10) {
  * }}
  */
 export async function createIdol(value) {
-  try {
-    const response = await commonInstance.post(`${teamName}/idols`, value);
-    if (response.status === HTTP_STATUS.STATUS_CREATED) return response.data;
-  } catch (err) {
-    console.log(err);
-    throw new Error("아이돌 생성 중 오류가 발생했습니다.");
-  }
+	try {
+		const response = await commonInstance.post(`/${teamName}/idols`, value);
+		if (response.status === HTTP_STATUS.STATUS_CREATED) return response.data;
+	} catch (err) {
+		console.log(err);
+		throw new Error("아이돌 생성 중 오류가 발생했습니다.");
+	}
 }
