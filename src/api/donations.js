d74e1c5f0c47695eec6f7b57ad62c6a5e2616892
callu9 <callu_9ine@naver.com>
2025-03-07 10:11:53 +0900

    chore: API 요청 함수 생성 및 수정

---
+++ src/api/donations.js
@@ -0,0 +1,101 @@
+import { commonInstance, HTTP_STATUS, teamName } from "./common-http";
+
+const PATH = "donations";
+
+/**
+ * 후원 목록 조회
+ * @param {number | undefined} cursor
+ * @param {number | undefined} pageSize
+ * @param {number[] | undefined} priorityIdolIds
+ * @returns {{
+ *  list: {
+ *    id: number,
+ *    idolId: number,
+ *    title: string,
+ *    subtitle: string,
+ *    targetDonation: number,
+ *    receivedDonations: number,
+ *    createdAt: string,
+ *    deadline: string,
+ *    status: boolean,
+ *    idol: { id: number, name: string, gender: "female" | "male", group: "string", profilePicture: string, totalVotes: number }
+ *  }[],
+ *  nextCursor: number | null
+ * }}
+ */
+export async function getDonations(cursor = 0, pageSize = 10, priorityIdolIds = []) {
+  const params = new URLSearchParams();
+  params.append("pageSize", pageSize);
+  if (cursor) params.append("cursor", cursor);
+  if (priorityIdolIds.length > 0)
+    for (const id of priorityIdolIds) {
+      params.append("priorityIdolIds", id);
+    }
+  try {
+    const response = await commonInstance.get(`/${teamName}/${PATH}/gender`, { params });
+    if (response.status === HTTP_STATUS.STATUS_OK) return response.data;
+  } catch (err) {
+    console.log(err);
+    throw new Error("후원 목록 조회 중 오류가 발생했습니다.");
+  }
+}
+/**
+ * 후원하기
+ * @param {number} id - 후원 ID
+ * @param {number} amount - 후원 금액
+ * @returns {{ id: number, title: string, subtitle: string, idolId: number, targetDonation: number, receivedDonations: number, createdAt: string, deadline: string, status: boolean }}
+ */
+export async function contributeDonation(id, amount) {
+  try {
+    const response = await commonInstance.put(`/${teamName}/${PATH}/${id}`, { amount });
+    if (response.status === HTTP_STATUS.STATUS_OK) return response.data;
+  } catch (err) {
+    console.log(err);
+    throw new Error("후원 생성 중 오류가 발생했습니다.");
+  }
+}
+// #region : Optional API
+/**
+ * 후원 생성
+ * @param {{ deadline: string, targetDonation: number, subtitle: string, title: string, idolId: number }} value
+ * @returns {{ id: number, title: string, subtitle: string, idolId: number, targetDonation: number, receivedDonations: number, createdAt: string, deadline: string, status: boolean, teamId: number }}
+ */
+export async function createDonation(value) {
+  try {
+    const response = await commonInstance.post(`/${teamName}/${PATH}`, value);
+    if (response.status === HTTP_STATUS.STATUS_CREATED) return response.data;
+  } catch (err) {
+    console.log(err);
+    throw new Error("후원 생성 중 오류가 발생했습니다.");
+  }
+}
+/**
+ * 후원 수정
+ * @param {number} id
+ * @param {{ deadline: string, targetDonation: number }} value
+ * @returns {{ id: number, title: string, subtitle: string, idolId: number, targetDonation: number, receivedDonations: number, createdAt: string, deadline: string, status: boolean }}
+ */
+export async function updateDonation(id, value) {
+  try {
+    const response = await commonInstance.put(`/${teamName}/${PATH}/${id}`, value);
+    if (response.status === HTTP_STATUS.STATUS_OK) return response.data;
+  } catch (err) {
+    console.log(err);
+    throw new Error("후원 생성 중 오류가 발생했습니다.");
+  }
+}
+/**
+ * 후원 삭제
+ * @param {number} id - 삭제할 후원 ID
+ * @returns {{ id: number, title: string, subtitle: string, idolId: number, targetDonation: number, receivedDonations: number, createdAt: string, deadline: string, status: boolean }}
+ */
+export async function deleteIdol(id) {
+  try {
+    const response = await commonInstance.delete(`/${teamName}/${PATH}/${id}`);
+    if (response.status === HTTP_STATUS.STATUS_OK) return response.data;
+  } catch (err) {
+    console.log(err);
+    throw new Error("후원 삭제 중 오류가 발생했습니다.");
+  }
+}
+// #endregion : Optional API
--- src/api/idols.js
+++ src/api/idols.js
@@ -1,38 +1,67 @@
 import { commonInstance, HTTP_STATUS, teamName } from "./common-http";
 
-export async function getIdolList(keyword, cursor = 1, pageSize = 10) {
-	const params = { keyword, cursor, pageSize };
-	try {
-		const response = await commonInstance.get(`/${teamName}/idols`, { params });
-		if (response.status === HTTP_STATUS.STATUS_OK) return response.data;
-	} catch (err) {
-		console.log(err);
-		throw new Error("아이돌 목록 조회 중 오류가 발생했습니다.");
-	}
-}
+const PATH = "idols";
+
 /**
- *
- * @param {{
- *  "profilePicture": string,
- *  "group": string,
- *  "gender": "female" | "male",
- *  "name": string
- * }} value
- * @returns {{
- *  "totalVotes": number,
- *  "profilePicture": string,
- *  "group": string,
- *  "gender": "female" | "male",
- *  "name": string,
- *  "id": number
- * }}
+ * 아이돌 목록 조회
+ * @param {string | undefined} keyword
+ * @param {number | undefined} cursor
+ * @param {number | undefined} pageSize
+ * @returns
+ */
+export async function getIdolList(keyword, cursor = 0, pageSize = 10) {
+  const params = { keyword, cursor, pageSize };
+  try {
+    const response = await commonInstance.get(`/${teamName}/${PATH}`, { params });
+    if (response.status === HTTP_STATUS.STATUS_OK) return response.data;
+  } catch (err) {
+    console.log(err);
+    throw new Error("아이돌 목록 조회 중 오류가 발생했습니다.");
+  }
+}
+
+// #region : Optional API
+/**
+ * 아이돌 생성
+ * @param {{ profilePicture: string, group: string, gender: "female" | "male", name: string }} value
+ * @returns {{ totalVotes: number, profilePicture: string, group: string, gender: "female" | "male", name: string, id: number }}
  */
 export async function createIdol(value) {
-	try {
-		const response = await commonInstance.post(`/${teamName}/idols`, value);
-		if (response.status === HTTP_STATUS.STATUS_CREATED) return response.data;
-	} catch (err) {
-		console.log(err);
-		throw new Error("아이돌 생성 중 오류가 발생했습니다.");
-	}
+  try {
+    const response = await commonInstance.post(`/${teamName}/${PATH}`, value);
+    if (response.status === HTTP_STATUS.STATUS_CREATED) return response.data;
+  } catch (err) {
+    console.log(err);
+    throw new Error("아이돌 생성 중 오류가 발생했습니다.");
+  }
 }
+/**
+ * 아이돌 수정
+ * @param {number} id
+ * @param {{ profilePicture: string, group: string, gender: "female" | "male", name: string }} value
+ * @returns {{ totalVotes: number, profilePicture: string, group: string, gender: "female" | "male", name: string, id: number }}
+ */
+export async function updateIdol(id, value) {
+  try {
+    const response = await commonInstance.put(`/${teamName}/${PATH}/${id}`, value);
+    if (response.status === HTTP_STATUS.STATUS_OK) return response.data;
+  } catch (err) {
+    console.log(err);
+    throw new Error("아이돌 정보 수정 중 오류가 발생했습니다.");
+  }
+}
+/**
+ * 아이돌 삭제
+ * @param {number} id - 삭제할 아이돌 ID
+ * @returns
+ */
+export async function deleteIdol(id) {
+  try {
+    const response = await commonInstance.delete(`/${teamName}/${PATH}/${id}`);
+    if (response.status === HTTP_STATUS.STATUS_OK) return response.data;
+  } catch (err) {
+    console.log(err);
+    throw new Error("아이돌 삭제 중 오류가 발생했습니다.");
+  }
+}
+// #endregion : Optional API
