96df1e1d00d540e85af5f3b4040f3796f2708d96
callu9 <callu_9ine@naver.com>
2025-03-10 08:46:34 +0900

    fix: Icon 컴포넌트 className 오류 수정 및 불필요한 이미지 import 제거

--- src/components/icon/Icon.jsx
+++ src/components/icon/Icon.jsx
@@ -4,10 +4,8 @@ import ButtonRight from "../../assets/btn/btn_pagination_arrow_right.svg";
 import Chevron from "../../assets/icon/ic_arrow_left.svg";
 import Chart from "../../assets/icon/ic_chart.svg";
 import Check from "../../assets/icon/ic_check.svg";
-import CreditWhite from "../../assets/icon/ic_credit.svg";
 import Delete from "../../assets/icon/ic_delete.svg";
 import Plus from "../../assets/icon/ic_plus_24px.svg";
-import Credit from "../../assets/img/credit.svg";
 import "./icon.scss";
 
 /**
@@ -17,28 +15,32 @@ import "./icon.scss";
  *  rotate: 0 | 90 | 180 | 270,
  * }} param
  */
-export default function Icon({ iconNm = "credit", size = 16, rotate = 0, className, ...props }) {
-	const ICONS = {
-		"button-left": ButtonLeft,
-		"button-right": ButtonRight,
-		chart: Chart,
-		check: Check,
-		chevron: Chevron,
-		close: Close,
-		credit: Credit,
-		"credit-white": CreditWhite,
-		delete: Delete,
-		plus: Plus,
-	};
+export default function Icon({
+  iconNm = "close",
+  size = 16,
+  rotate = 0,
+  className = "",
+  ...props
+}) {
+  const ICONS = {
+    "button-left": ButtonLeft,
+    "button-right": ButtonRight,
+    chart: Chart,
+    check: Check,
+    chevron: Chevron,
+    close: Close,
+    delete: Delete,
+    plus: Plus,
+  };
 
-	return iconNm === "credit" || iconNm === "credit-white" ? (
-		<div className={`icon-${iconNm} icon-${size} icon-rotate-${rotate} ${className}`} />
-	) : (
-		<img
-			src={ICONS[iconNm]}
-			className={`icon-${iconNm} icon-${size} icon-rotate-${rotate} ${className}`}
-			alt="아이콘 이미지"
-			{...props}
-		/>
-	);
+  return iconNm.startsWith("credit") ? (
+    <div className={`icon-${iconNm} icon-${size} icon-rotate-${rotate} ${className}`} />
+  ) : (
+    <img
+      src={ICONS[iconNm]}
+      className={`icon-${iconNm} icon-${size} icon-rotate-${rotate} ${className}`}
+      alt="아이콘 이미지"
+      {...props}
+    />
+  );
 }
