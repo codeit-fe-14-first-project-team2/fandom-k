725a5034a0a368c50e361cf1197fa8abbda35554
callu9 <callu_9ine@naver.com>
2025-03-11 14:30:19 +0900

    refactor: API 인터페이스 설정 통한 API 요청 리팩토링

--- src/api/charts.js
+++ src/api/charts.js
@@ -1,19 +1,7 @@
-import { commonInstance, HTTP_STATUS, teamName } from "./common-http";
+import { handler } from "./class-http-client";
 
-const PATH = "votes";
-
-/**
- * @param {"female" | "male"} gender
- * @param {number | undefined} cursor
- * @param {number | undefined} pageSize
- */
-export async function getRankedChart(gender, cursor = 0, pageSize = 10) {
-  const params = { gender, cursor, pageSize };
-  try {
-    const response = await commonInstance.get(`/${teamName}/${PATH}/gender`, { params });
-    if (response.status === HTTP_STATUS.STATUS_OK) return response.data;
-  } catch (err) {
-    console.log(err);
-    throw new Error("이달의 차트 조회 중 오류가 발생했습니다.");
-  }
+const PATH = "charts";
+export async function getChart({ selectedTab, cursor = 0, pageSize = 10 }) {
+  const params = { gender: selectedTab, cursor, pageSize };
+  return await handler.get(`${PATH}/${selectedTab}`, params);
 }
--- src/api/image.js
+++ src/api/image.js
@@ -1,11 +1,5 @@
-import { HTTP_STATUS, multipartInstance } from "./common-http";
+import { handler } from "./class-http-client";
 
 export async function uploadImage(file) {
-  try {
-    const response = await multipartInstance.post("/images/upload", file);
-    if (response.status === HTTP_STATUS.STATUS_CREATED) return response.data;
-  } catch (err) {
-    console.log(err);
-    throw new Error("이미지 업로드 중 오류가 발생했습니다.");
-  }
+  return await handler.uploadImage(file);
 }
