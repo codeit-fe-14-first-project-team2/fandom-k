638143955952f699f3d498457f5db1f59b84ebab
ShiroUsagi25 <irrukaboy@gmail.com>
2025-03-11 15:40:14 +0900

    feat: mypage 전체적인 코드 생성

---
+++ src/hooks/useDataNum.jsx
@@ -0,0 +1,40 @@
+import { useState, useEffect } from 'react';
+import { SIZES } from '../utils/Constants';
+
+function useDataNum(items = { mobile: 6, tablet: 8, desktop: 16 }) {
+    const [dataNum, setDataNum] = useState(() => {
+        const width = window.innerWidth;
+        if (width <= SIZES.mobile) {
+            return items.mobile;
+        } else if (width <= SIZES.tablet) {
+            return items.tablet;
+        } else {
+            return items.desktop;
+        }
+    });
+
+    // 화면 크기에 따라 아이템 개수를 업데이트하는 함수
+    const updateDataNum = () => {
+        const width = window.innerWidth;
+        if (width <= SIZES.mobile) {
+            setDataNum(items.mobile); // 모바일 화면일 때
+        } else if (width <= SIZES.tablet) {
+            setDataNum(items.tablet); // 태블릿 화면일 때
+        } else {
+            setDataNum(items.desktop); // 데스크탑 화면일 때
+        }
+    };
+
+    useEffect(() => {
+        window.addEventListener('resize', updateDataNum);
+
+        // 컴포넌트 언마운트 시 이벤트 리스너 제거
+        return () => {
+            window.removeEventListener('resize', updateDataNum);
+        };
+    }, []);
+
+    return dataNum;
+}
+
+export default useDataNum;
---
+++ src/hooks/useScrollTo.jsx
@@ -0,0 +1,19 @@
+import { useRef } from 'react';
+
+const useScrollTo = () => {
+    const ref = useRef(null);
+
+    const scrollTo = (direction, offset = 24) => {
+        if (!ref.current) return;
+
+        const scrollAmount = ref.current.offsetWidth + offset;
+        ref.current.scrollBy({
+            left: direction === 'next' ? scrollAmount : -scrollAmount,
+            behavior: 'smooth',
+        });
+    };
+
+    return { ref, scrollTo };
+};
+
+export default useScrollTo;
