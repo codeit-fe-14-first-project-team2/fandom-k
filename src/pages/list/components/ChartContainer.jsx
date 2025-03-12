4b55fe01107c58abfc85500e479caef95c088f1a
ghdtnals <hongsumin58@gmail.com>
2025-03-12 17:05:49 +0900

    remove: IdolListItem 컴포넌트 제거

--- src/pages/list/components/ChartContainer.jsx
+++ src/pages/list/components/ChartContainer.jsx
@@ -1,8 +1,8 @@
 import { useEffect, useState } from "react";
 import Button from "../../../components/button/Button";
 import Chart from "../../../assets/icon/ic_chart.svg";
-import IdolListItem from "./IdolListItem";
 import { getChart } from "../../../api/charts";
+import IdolProfile from "../../../components/idolprofile/IdolProfile";
 import VoteModal from "../../../modal/VoteModal";
 import { useSetModal } from "../../../contexts/GlobalContext";
 
@@ -15,6 +15,21 @@ export default function ChartContainer() {
   const [cursor, setCursor] = useState(null);
   const [loading, setLoading] = useState(false);
 
+  function IdolListItem({ id, rank, group, name, totalVotes, profilePicture }) {
+    return (
+      <li className="display-flex justify-sides align-center">
+        <div className="display-flex justify-sides align-center gap-12">
+          <IdolProfile profilePicture={profilePicture} name={name} id={id} size="small" />
+          <span className="text-regular text-16 text-brand-orange">{rank}</span>
+          <span className="text-medium text-16">
+            {group} {name}
+          </span>
+        </div>
+
+        <span className="text-regular text-16 text-invert-60">{totalVotes.toLocaleString()}표</span>
+      </li>
+    );
+  }
   async function handleLoad(options, reset = false) {
     setLoading(true);
     try {
