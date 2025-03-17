922f4e48647db82e4e3eff28ab23d7560cb8765e
callu9 <callu_9ine@naver.com>
2025-03-17 13:41:43 +0900

    fix: 빌드 오류 수정 (path error)

--- src/components/icon/Icon.jsx
+++ src/components/icon/Icon.jsx
@@ -1,4 +1,3 @@
-
 import Close from "../../assets/btn/btn_delete_24px.svg";
 import ButtonLeft from "../../assets/btn/btn_pagination_arrow_left.svg";
 import ButtonRight from "../../assets/btn/btn_pagination_arrow_right.svg";
@@ -6,7 +5,7 @@ import Chevron from "../../assets/icon/ic_arrow_left.svg";
 import Chart from "../../assets/icon/ic_chart.svg";
 import Check from "../../assets/icon/ic_check.svg";
 import Delete from "../../assets/icon/ic_delete.svg";
-import Plus from "../../assets/icon/ic_plus_24px.svg";
+import Plus from "../../assets/icon/Ic_plus_24px.svg";
 import "./icon.scss";
 
 /**
@@ -17,31 +16,31 @@ import "./icon.scss";
  * }} param
  */
 export default function Icon({
-  iconNm = "close",
-  size = 16,
-  rotate = 0,
-  className = "",
-  ...props
+	iconNm = "close",
+	size = 16,
+	rotate = 0,
+	className = "",
+	...props
 }) {
-  const ICONS = {
-    "button-left": ButtonLeft,
-    "button-right": ButtonRight,
-    chart: Chart,
-    check: Check,
-    chevron: Chevron,
-    close: Close,
-    delete: Delete,
-    plus: Plus,
-  };
+	const ICONS = {
+		"button-left": ButtonLeft,
+		"button-right": ButtonRight,
+		chart: Chart,
+		check: Check,
+		chevron: Chevron,
+		close: Close,
+		delete: Delete,
+		plus: Plus,
+	};
 
-  return iconNm.startsWith("credit") ? (
-    <div className={`icon-${iconNm} icon-${size} icon-rotate-${rotate} ${className}`} {...props} />
-  ) : (
-    <img
-      src={ICONS[iconNm]}
-      className={`icon-${iconNm} icon-${size} icon-rotate-${rotate} ${className}`}
-      alt="아이콘 이미지"
-      {...props}
-    />
-  );
+	return iconNm.startsWith("credit") ? (
+		<div className={`icon-${iconNm} icon-${size} icon-rotate-${rotate} ${className}`} {...props} />
+	) : (
+		<img
+			src={ICONS[iconNm]}
+			className={`icon-${iconNm} icon-${size} icon-rotate-${rotate} ${className}`}
+			alt="아이콘 이미지"
+			{...props}
+		/>
+	);
 }
